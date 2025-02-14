62bf759a1d3ace04a6a905900e8d3512392dda9e
Halsey Svetz <halseycheese@gmail.com>
2025-02-14 13:31:21 -0700

    add update functionality

--- server.js
+++ server.js
@@ -59,7 +59,15 @@ res.render("fruits/edit.ejs", {
 });
 });
 
-
+app.put("/fruits/:fruitId", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+    res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 
 
